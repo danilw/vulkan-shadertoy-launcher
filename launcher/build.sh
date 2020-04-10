9971df0f8837792ea3de715a57e7104726cf1b6f
Danil <s48gs.w@gmail.com>
2020-04-10 06:27:46 +0300

    Delete build.sh

--- launcher/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gcc -DVK_USE_PLATFORM_XCB_KHR -O2 -s -lm -lxcb -lxcb-keysyms -lvulkan ../vk_utils/vk_utils.c ../vk_utils/vk_error_print.c ../vk_utils/vk_render_helper.c main.c -o VKexample
