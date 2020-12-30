523f8fdde3f98ed5666683867ec7831e242fdeec
Danil <s48gs.w@gmail.com>
2020-12-30 16:41:42 +0300

    Update vk_error_print.h

--- vk_utils/vk_error_print.h
+++ vk_utils/vk_error_print.h
@@ -1,26 +1,6 @@
 
 // Danil, 2020 Vulkan shader launcher, self https://github.com/danilw/vulkan-shader-launcher
-// code based on Shabi's Vulkan Tutorials https://github.com/ShabbyX/vktut
-
-// original license
-/*
- * Copyright (C) 2016 Shahbaz Youssefi <ShabbyX@gmail.com>
- *
- * This file is part of Shabi's Vulkan Tutorials.
- *
- * Shabi's Vulkan Tutorials is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Shabi's Vulkan Tutorials is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Shabi's Vulkan Tutorials.  If not, see <http://www.gnu.org/licenses/>.
- */
+// The MIT License
 
 #ifndef vk_utils_printf_H
 #define vk_utils_printf_H
