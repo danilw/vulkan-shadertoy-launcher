becc6a847b3f26232607672f88739a5e63f025d5
Danil <s48gs.w@gmail.com>
2021-06-01 17:59:17 +0300

    Ubuntu note

--- build_scripts/validation/VK_validation.sh
+++ build_scripts/validation/VK_validation.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-VULKAN_SDK="<path to Vulkan SDK/version>" #example /home/user/vulkan_sdk/1.2.131.2/
+## this script DOES NOT WORK in the Ubuntu 21.04, copy paste these commands to the console
+## and launch Vulkan app manually from console to have Validation layers work
+
+## path example /home/user/vulkan_sdk/1.2.131.2/
+VULKAN_SDK="<path to Vulkan SDK/version>"
 VK_APP=$1
 
 . $VULKAN_SDK"/setup-env.sh"
