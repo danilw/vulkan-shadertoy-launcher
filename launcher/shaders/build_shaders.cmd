ce1454035546640f9dfed8ad809deb88d66977df
Danil <s48gs.w@gmail.com>
2024-02-24 15:33:44 +0300

    Update build_shaders.cmd

--- launcher/shaders/build_shaders.cmd
+++ launcher/shaders/build_shaders.cmd
@@ -7,5 +7,5 @@ glslangValidator.exe -V100 src/buf1.frag -o spv/buf1.frag.spv
 glslangValidator.exe -V100 src/buf2.frag -o spv/buf2.frag.spv
 glslangValidator.exe -V100 src/buf3.frag -o spv/buf3.frag.spv
 
-echo "Compiling done, if no errors close this."
+echo "Compiling shaders done, if no errors close this."
 pause
