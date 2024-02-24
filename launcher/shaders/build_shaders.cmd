612a9ba5e3311be2162eeca181face2a3d02b46e
Danil <s48gs.w@gmail.com>
2024-02-24 15:33:15 +0300

    Update build_shaders.cmd

--- launcher/shaders/build_shaders.cmd
+++ launcher/shaders/build_shaders.cmd
@@ -1,9 +1,11 @@
-#!/bin/sh
 
-glslangValidator -V100 src/main.vert -o spv/main.vert.spv
-glslangValidator -V100 src/main.frag -o spv/main.frag.spv
-glslangValidator -V100 src/buf.vert -o spv/buf.vert.spv
-glslangValidator -V100 src/buf.frag -o spv/buf.frag.spv
-glslangValidator -V100 src/buf1.frag -o spv/buf1.frag.spv
-glslangValidator -V100 src/buf2.frag -o spv/buf2.frag.spv
-glslangValidator -V100 src/buf3.frag -o spv/buf3.frag.spv
+glslangValidator.exe -V100 src/main.vert -o spv/main.vert.spv
+glslangValidator.exe -V100 src/main.frag -o spv/main.frag.spv
+glslangValidator.exe -V100 src/buf.vert -o spv/buf.vert.spv
+glslangValidator.exe -V100 src/buf.frag -o spv/buf.frag.spv
+glslangValidator.exe -V100 src/buf1.frag -o spv/buf1.frag.spv
+glslangValidator.exe -V100 src/buf2.frag -o spv/buf2.frag.spv
+glslangValidator.exe -V100 src/buf3.frag -o spv/buf3.frag.spv
+
+echo "Compiling done, if no errors close this."
+pause
