68ea9d8e3f37088b5aef2a31772b235d374e8ace
Danil <s48gs.w@gmail.com>
2025-11-25 07:29:54 +0300

    Add files via upload

--- build_scripts/validation/VK_validation.sh
+++ build_scripts/validation/VK_validation.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+## sh VK_validation.sh VK_shadertoy
+
 ## this script DOES NOT WORK in the Ubuntu 21.04, copy paste these commands to the console
 ## and launch Vulkan app manually from console to have Validation layers work
 
 ## path example /home/user/vulkan_sdk/1.3.216.0
-VULKAN_SDK="/home/danil/Documents/git/vulkan_sdk/1.3.216.0"
+VULKAN_SDK="/home/danil/vulkan_sdk/1.4.328.1"
 VK_APP=$1
 
 . $VULKAN_SDK"/setup-env.sh"
